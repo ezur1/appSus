6211d02d4281c2b4771860f4a655b040d2d0867c
natan-git <natanyagodayv@gmail.com>
2019-11-10 16:56:05 +0200

    added img and video edit features

--- js/apps/mr-keep/cmp/video-keep.cmp.js
+++ js/apps/mr-keep/cmp/video-keep.cmp.js
@@ -5,9 +5,14 @@ export default {
     template: `
         <section class="card-container flex col" :class="cardColor">
         <iframe class="video-note" :src='imgUrl'></iframe>
+        <div class="edit-text flex wrap">
+            <textarea v-model="txt" class="edit-txt-input" ref="editInput" v-if="edit" type="text" :placeholder="placeholder"/>
+            <i @click="onAddUpdatedKeep(id)" v-if="edit" class="fas fa-plus"></i>
+        </div>
         <div class="flex align-end card-icons">
             <i @click="pinKeep(id)" class="fas fa-thumbtack" :class="{pinColor:pinnedColor}"></i>
             <i @click="showColors()" class="fas fa-palette"></i>
+            <i @click="onEditKeep(id)"  class="far fa-edit"></i>
             <i @click="deleteKeep(id)" class="fas fa-trash-alt "></i>
         </div>
         <color-picker class="color-picker animated bounce" v-show="isShow"  @changeColor="setBackgroundColor"></color-picker>
@@ -19,7 +24,9 @@ export default {
             videoUrl: 'http://www.youtube.com/embed/',
             cardColor: '',
             isShow: false,
-            pinnedColor: null
+            pinnedColor: null,
+            edit: false,
+            txt: ''
         }
     },
     created() {},
@@ -44,7 +51,15 @@ export default {
         pinKeep(id) {
             this.pinnedColor = !this.pinnedColor;
             keepService.setPin(id);
-        }
+        },
+        onEditKeep() {
+            this.edit = true;
+        },
+        onAddUpdatedKeep(keepId) {
+            let newContent = this.$refs.editInput.value
+            keepService.saveUpdatedKeep(keepId, newContent)
+            this.edit = false;
+        },
     },
     components: {
         colorPicker,
