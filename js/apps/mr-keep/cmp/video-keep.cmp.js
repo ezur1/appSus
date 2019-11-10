c8e443c184bb4a40a6cae2f6991262878e28d226
natan-git <natanyagodayv@gmail.com>
2019-11-10 17:32:12 +0200

    fix youtube url

--- js/apps/mr-keep/cmp/video-keep.cmp.js
+++ js/apps/mr-keep/cmp/video-keep.cmp.js
@@ -21,7 +21,7 @@ export default {
     props: ['data', 'id'],
     data() {
         return {
-            videoUrl: 'http://www.youtube.com/embed/',
+            videoUrl: 'https://www.youtube.com/embed/',
             cardColor: '',
             isShow: false,
             pinnedColor: null,
